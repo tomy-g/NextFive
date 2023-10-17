44bfe5ca9a7693d3bc5fff855e583d02575c5af3
Tomás <tomasgoizuetadp@gmail.com>
2023-10-17 22:01:11 +0200

    Next UI working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
-import Image from 'next/image'
+import { Button } from '@nextui-org/button'
 
 export default function Home () {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className="flex flex-wrap gap-4 items-center">
+        <Button color="default">
+          Default
+        </Button>
+        <Button color="primary">
+          Primary
+        </Button>
+        <Button color="secondary">
+          Secondary
+        </Button>
+        <Button color="success">
+          Success
+        </Button>
+        <Button color="warning">
+          Warning
+        </Button>
+        <Button color="danger">
+          Danger
+        </Button>
+      </div>
     </main>
   )
 }
---
+++ src/components/reactivo.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Button } from '@nextui-org/button'
+
+const Reactivo = () => {
+  return (
+    <Button color='primary'>Button</Button>
+  )
+}
+
+export default Reactivo
