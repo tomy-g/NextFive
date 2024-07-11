e5a1b6ebb51fd2ad3e64f5811683cc5e42010eb0
Tomás <tomasgoizuetadp@gmail.com>
2024-07-11 12:58:17 +0200

    feat: add rate limit to ai api

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,13 +1,29 @@
 import { streamObject } from 'ai'
 import { openai } from '@ai-sdk/openai'
-import { moviesSchema } from '@/app/schemas/movies'
+import { kv } from '@vercel/kv'
+import { Ratelimit } from '@upstash/ratelimit'
+import { moviesSchema } from '@/app/schemas/movie'
+import { type NextRequest } from 'next/server'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
 
-export async function POST (req: Request) {
-  const context = await req.json()
+// Create Rate limit
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.fixedWindow(5, '30s'),
+})
 
+export async function POST (req: NextRequest) {
+  // call ratelimit with request ip
+  const ip = req.ip ?? 'ip'
+  const { success } = await ratelimit.limit(ip)
+  // block the request if unsuccessfull
+  if (!success) {
+    return new Response('Ratelimited!', { status: 429 })
+  }
+
+  const context = await req.json()
   const result = await streamObject({
     model: openai('gpt-3.5-turbo'),
     schema: moviesSchema,
@@ -18,16 +34,3 @@ export async function POST (req: Request) {
 
   return result.toTextStreamResponse()
 }
-
-// export async function POST (req: Request) {
-//   const { prompt }: { prompt: string } = await req.json()
-
-//   const result = await generateObject({
-//     model: openai('gpt-4-turbo'),
-//     system: 'You are a movie recommendation system. You are an expert on movies and TV series.',
-//     prompt,
-//     schema: moviesSchema
-//   })
-
-//   return result.toJsonResponse()
-// }
