c2b64cc338b716ffd27f46a70d6fee61fd1cda17
Tomás <tomasgoizuetadp@gmail.com>
2024-07-21 17:10:30 +0200

    fix: link on empty cards

--- src/components/SelectedMovie.tsx
+++ src/components/SelectedMovie.tsx
@@ -44,7 +44,8 @@ function SelectedMovie ({ movie, deselectMovie }: Props) {
           className=''
           color='secondary'
         >
-          <Link href={`/movie/${movie.imdbID}`}>
+          {movie.imdbID.length < 2
+            ? (
             <Card className='rounded-md'>
               <Image
                 as={NextImage}
@@ -82,7 +83,48 @@ function SelectedMovie ({ movie, deselectMovie }: Props) {
                   : null}
               </CardFooter>
             </Card>
-          </Link>
+              )
+            : (
+            <Link href={`/movie/${movie.imdbID}`}>
+              <Card className='rounded-md'>
+                <Image
+                  as={NextImage}
+                  priority
+                  objectFit='cover'
+                  className='rounded-md aspect-[0.675/1]'
+                  width={300}
+                  height={448}
+                  src={
+                    movie.Poster !== undefined &&
+                    movie.Poster !== 'N/A' &&
+                    movie.Poster.length > 0
+                      ? movie.Poster
+                      : movie.Poster === 'N/A'
+                        ? nopicture.src
+                        : placeholder.src
+                  }
+                ></Image>
+                <CardFooter className='absolute z-10 bottom-0 text-small pb-0 sm:pb-3'>
+                  {movie.Poster === 'N/A' ? <span>{movie.Title}</span> : null}
+                  {isFilled(movie)
+                    ? (
+                    <Button
+                      isIconOnly
+                      variant='light'
+                      radius='full'
+                      className='mx-auto opacity-75 hover:text-danger hover:opacity-100'
+                      onPress={() => {
+                        deselectMovie(movie)
+                      }}
+                    >
+                      <X />
+                    </Button>
+                      )
+                    : null}
+                </CardFooter>
+              </Card>
+            </Link>
+              )}
         </Tooltip>
       </Skeleton>
     </li>
