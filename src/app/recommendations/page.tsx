d3634e90bbab1b5eac744f33be8e4c927f24e882
Tomás <tomasgoizuetadp@gmail.com>
2024-07-25 14:34:15 +0200

    feat: change to nextui navigation in header and replace svg

---
+++ src/app/recommendations/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Recommendations () {
+  return (
+    <main className='mt-16'>page</main>
+  )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { Image, Link } from '@nextui-org/react'
+import { Link } from '@nextui-org/react'
 import LinkNext from 'next/link'
-import NextImage from 'next/image'
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem
 } from '@nextui-org/navbar'
-import nextFive from '@/app/assets/nextFive.svg'
+import NextFiveSVG from './NextFiveSVG'
 
 export default function Header () {
   return (
-    // <header className='pt-5 w-96 lg:w-[60rem]'>
-    //   <a href='/'>
-    //     <Image src='nextFive.svg' className='w-32 aspect-[568/174] ' radius='none' as={NextImage} width={568} height={174} />
-    //   </a>
     <Navbar
       classNames={{
         wrapper: 'px-0'
@@ -23,18 +18,15 @@ export default function Header () {
     >
       <LinkNext href='/'>
         <NavbarBrand>
-          <Image
-            src={nextFive.src}
-            as={NextImage}
-            className='w-32 aspect-[568/174] '
-            priority
-            radius='none'
-            width={568}
-            height={174}
-          />
+          <NextFiveSVG />
         </NavbarBrand>
       </LinkNext>
       <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        <NavbarItem>
+          <Link color='foreground' href='recommendations'>
+            Recommendations
+          </Link>
+        </NavbarItem>
         <NavbarItem>
           <Link color='foreground' href='#'>
             Help
@@ -45,13 +37,8 @@ export default function Header () {
             Settings
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color='foreground' href='#'>
-            Integrations
-          </Link>
-        </NavbarItem>
       </NavbarContent>
     </Navbar>
-    /* </header> */
+
   )
 }
